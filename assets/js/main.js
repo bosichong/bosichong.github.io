88ead8bba0868a9d9c46e7b46b08efdac0b02bf6
J.sky <bosichong@qq.com>
2023-05-13 10:05:59 +0800

    更新样式

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,8 +86,8 @@
    */
   on('click', '.mobile-nav-toggle', function (e) {
     select('body').classList.toggle('mobile-nav-active')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
+    this.classList.toggle('fa-navicon')
+    this.classList.toggle('fa-close')
   })
 
   document.addEventListener('DOMContentLoaded', (event) => {
