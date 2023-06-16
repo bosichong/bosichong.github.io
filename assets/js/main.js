0c5bc5e14a34e474a4af4f976cd240ba63b4fae6
J.sky <bosichong@qq.com>
2023-06-16 11:25:06 +0800

    更换全新的博客主题

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,99 +1,24 @@
 
 (function () {
+  
 
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
+  const sider = document.getElementById("sider");
+  const menuOpenBtn = document.getElementById("menu-open-btn");
+  const menuCloseBtn = document.getElementById("menu-close-btn");
+
+  menuOpenBtn.addEventListener("click", () => {
+    toggleMenu();
+  });
+
+  menuCloseBtn.addEventListener("click", () => {
+    toggleMenu();
+  });
+
+  function toggleMenu() {
+    sider.classList.toggle("sider-top");
   }
 
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
 
-  /**
-   * Easy on scroll event listener 
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
-
-  /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
-
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos,
-      behavior: 'smooth'
-    })
-  }
-
-  /**
-   * Back to top button
-   */
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
-
-  /**
-   * Mobile nav toggle
-   */
-  on('click', '.mobile-nav-toggle', function (e) {
-    select('body').classList.toggle('mobile-nav-active')
-    this.classList.toggle('animate__bounceOut')
-    setTimeout(()=>{
-      this.classList.toggle('animate__bounceOut')
-      this.classList.toggle('fa-navicon')
-      this.classList.toggle('fa-close')
-  },1000)
-    
-  })
 
   document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('pre code').forEach((el) => {
@@ -103,81 +28,35 @@
 
 
 
-  // 为文档中的图片添加自适应class样式
-  const images = document.querySelectorAll('.inner-page img');
-  for (let i = 0; i < images.length; i++) {
-    images[i].classList.add('img-fluid');
-  }
-
 
   // 全站搜索
   const blog_url = suiyan.url
   const searchInput = document.getElementById('search-input');
   const searchBtn = document.getElementById('search-btn');
-  const modal = document.getElementById('modal');
   const resultList = document.getElementById('result-list');
+
+  const searchInput1 = document.getElementById('search-input1');
+  const searchBtn1 = document.getElementById('search-btn1');
+  const resultList1 = document.getElementById('result-list1');
 
   // 读取JSON文件
   fetch(blog_url + 'blog_data.json')
     .then(response => response.json())
     .then(data => {
       // 处理搜索按钮点击事件
-      searchBtn.addEventListener('click', () => {
-        const keyword = searchInput.value.trim().toLowerCase();
-        const filteredArticles = data.filter(article => {
-          return article.title.toLowerCase().includes(keyword);
-        });
-        // 清空搜索结果列表
-        resultList.innerHTML = '';
-        // 添加搜索结果到列表中
-        if (filteredArticles.length === 0) {
-          const li = document.createElement('li');
-          li.textContent = '没有搜到相关内容';
-          resultList.appendChild(li);
-          modal.style.display = 'block';
-        } else {
-          filteredArticles.forEach(article => {
-            const li = document.createElement('li');
-            const a = document.createElement('a');
-            a.href = blog_url + article.url + ".html";
-            a.target = '_blank';
-            a.textContent = article.title;
-            li.appendChild(a);
-            resultList.appendChild(li);
-          });
-          modal.style.display = 'block';
-        }
+      searchBtn.addEventListener('click', (e) => {
+        showRst(searchInput, data, resultList, blog_url);
+        e.preventDefault()
+        return false;
+      });
+
+      searchBtn1.addEventListener('click', (e) => {
+        showRst(searchInput1, data, resultList1, blog_url);
+        e.preventDefault()
+        return false;
       });
     });
 
-  // 当#search-input获取焦点时，删除#search-btn的.vibrate-1类
-  searchInput.addEventListener('focus', () => {
-    searchBtn.classList.remove('vibrate-1');
-  });
-
-  // 当#search-input失去焦点时，#search-btn添加.vibrate-1类
-  searchInput.addEventListener('blur', () => {
-    searchBtn.classList.add('vibrate-1');
-  });
-
-
-  const links = document.querySelectorAll('a');
-  links.forEach(link => {
-    link.addEventListener('mouseover', () => {
-      // 在鼠标悬停在链接上时添加你的代码
-      link.classList.add('vibrate-1')
-    });
-    link.addEventListener('mouseout', () => {
-      // 在鼠标离开链接时添加你的代码
-      link.classList.remove('vibrate-1')
-    });
-  });
-
-  // bootstrap5 文字提示工具初始化
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-  })
 
 })()
 
@@ -191,5 +70,48 @@ $(function () {
     });
     $('.up-time').each(function (i, e) {
         $(e).text(dayjs($(e).text()).fromNow())  
+    });
+});
+function showRst(searchInput, data, resultList, blog_url) {
+  const keyword = searchInput.value.trim().toLowerCase();
+  const filteredArticles = data.filter(article => {
+    return article.title.toLowerCase().includes(keyword);
+  });
+  // 清空搜索结果列表
+  resultList.innerHTML = '';
+  // 添加搜索结果到列表中
+  if (filteredArticles.length === 0) {
+    const li = document.createElement('li');
+    li.textContent = '没有搜到相关内容';
+    resultList.appendChild(li);
+  } else {
+    filteredArticles.forEach(article => {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.href = blog_url + article.url + ".html";
+      a.target = '_blank';
+      a.textContent = article.title;
+      li.appendChild(a);
+      resultList.appendChild(li);
+    });
+  }
+}
+
+$(function () {
+    $.scrollUp({
+        scrollName: 'scrollUp',      // Element ID
+        scrollDistance: 300,         // Distance from top/bottom before showing element (px)
+        scrollFrom: 'top',           // 'top' or 'bottom'
+        scrollSpeed: 300,            // Speed back to top (ms)
+        easingType: 'linear',        // Scroll to top easing (see http://easings.net/)
+        animation: 'fade',           // Fade, slide, none
+        animationSpeed: 200,         // Animation speed (ms)
+        scrollTrigger: false,        // Set a custom triggering element. Can be an HTML string or jQuery object
+        scrollTarget: false,         // Set a custom target element for scrolling to. Can be element or number
+        scrollText: '<i class="fa fa-arrow-circle-o-up"></i>', // Text for element, can contain HTML
+        scrollTitle: false,          // Set a custom <a> title if required.
+        scrollImg: false,            // Set true to use image
+        activeOverlay: false,        // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+        zIndex: 999           // Z-Index for the overlay
     });
 });
