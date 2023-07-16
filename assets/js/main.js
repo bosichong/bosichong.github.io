be65059a3d7b260687da973e5870eb294d71968e
J.sky <bosichong@qq.com>
2023-07-16 23:48:21 +0800

    更新博客

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,4 @@
 
-(function () {
-  
-
-  const sider = document.getElementById("sider");
-  const menuOpenBtn = document.getElementById("menu-open-btn");
-  const menuCloseBtn = document.getElementById("menu-close-btn");
-
-  menuOpenBtn.addEventListener("click", () => {
-    toggleMenu();
-  });
-
-  menuCloseBtn.addEventListener("click", () => {
-    toggleMenu();
-  });
-
-  function toggleMenu() {
-    sider.classList.toggle("sider-top");
-  }
-
-
-
-  document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelectorAll('pre code').forEach((el) => {
-      hljs.highlightElement(el);
-    });
-  });
-
-
-
-})()
 
 //文章时间的格式化。
 // moment.locale('zh-CN');
@@ -45,22 +15,3 @@ $(function () {
 
 
 
-
-$(function () {
-    $.scrollUp({
-        scrollName: 'scrollUp',      // Element ID
-        scrollDistance: 300,         // Distance from top/bottom before showing element (px)
-        scrollFrom: 'top',           // 'top' or 'bottom'
-        scrollSpeed: 300,            // Speed back to top (ms)
-        easingType: 'linear',        // Scroll to top easing (see http://easings.net/)
-        animation: 'fade',           // Fade, slide, none
-        animationSpeed: 200,         // Animation speed (ms)
-        scrollTrigger: false,        // Set a custom triggering element. Can be an HTML string or jQuery object
-        scrollTarget: false,         // Set a custom target element for scrolling to. Can be element or number
-        scrollText: '<i class="fa fa-arrow-up"></i>', // Text for element, can contain HTML
-        scrollTitle: false,          // Set a custom <a> title if required.
-        scrollImg: false,            // Set true to use image
-        activeOverlay: false,        // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-        zIndex: 999           // Z-Index for the overlay
-    });
-});
