e5175ea17ff49ff4236dac1a29affa4724b49c5f
J.sky <bosichong@qq.com>
2023-07-17 09:06:39 +0800

    更新博客

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,21 @@
 
 
 //文章时间的格式化。
-// moment.locale('zh-CN');
 dayjs.locale('zh-cn')
 dayjs.extend(window.dayjs_plugin_relativeTime)
-$(function () {
-    $('.cr-time').each(function (i, e) {
-        $(e).text(dayjs($(e).text()).fromNow())
-    });
-    $('.up-time').each(function (i, e) {
-        $(e).text(dayjs($(e).text()).fromNow())  
-    });
-});
+window.onload = function () {
+  var crTimes = document.querySelectorAll(".cr-time");
+  var upTimes = document.querySelectorAll(".up-time");
+
+  for (var i = 0; i < crTimes.length; i++) {
+    var crTime = crTimes[i];
+    crTime.textContent = dayjs(crTime.textContent).fromNow();
+  }
+
+  for (var i = 0; i < upTimes.length; i++) {
+    var upTime = upTimes[i];
+    upTime.textContent = dayjs(upTime.textContent).fromNow();
+  }
 
 
-
+};
--- assets/js/tag.js
+++ assets/js/tag.js
@@ -1,9 +1,47 @@
-$(document).ready(function () {
-    $('.tag').click(function (e) { 
-        $(this).siblings('.tag').fadeToggle(500);
-        let tagdata = $(this).attr('data-tag')
-        console.log(tagdata);
-        $('.'+tagdata).fadeToggle(500);
+document.addEventListener('DOMContentLoaded', function() {
+    var tags = document.querySelectorAll('.tag');
+    for (var i = 0; i < tags.length; i++) {
+      tags[i].addEventListener('click', function(e) {
+        var siblings = getSiblings(this, '.tag');
+        hideElements(siblings);
         
-    });
-});
+        var ul = this.nextElementSibling;
+        toggleElement(ul);
+  
+        if (ul.style.display === 'none') {
+          showElements(tags);
+        }
+      });
+    }
+  });
+  
+  function getSiblings(element, selector) {
+    var siblings = [];
+    var sibling = element.parentNode.firstChild;
+    for (; sibling; sibling = sibling.nextSibling) {
+      if (sibling.nodeType !== 1 || sibling === element) continue;
+      if (sibling.matches(selector)) siblings.push(sibling);
+    }
+    return siblings;
+  }
+  
+  function hideElements(elements) {
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.display = 'none';
+    }
+  }
+  
+  function showElements(elements) {
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.display = '';
+    }
+  }
+  
+  function toggleElement(element) {
+    if (element.style.display === 'none') {
+      element.style.display = '';
+    } else {
+      element.style.display = 'none';
+    }
+  }
+  
