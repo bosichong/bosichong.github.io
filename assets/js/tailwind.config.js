2a7aca272ae253169ad2260d9bd15fe4ac3b3a23
J.sky <bosichong@qq.com>
2023-07-22 10:24:54 +0800

    更新博客

--- assets/js/tailwind.config.js
+++ assets/js/tailwind.config.js
@@ -5,7 +5,7 @@ tailwind.config = {
       "cupcake",
       "dark",
     ],
-    darkTheme: "cupcake", // name of one of the included themes for dark mode
+    darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
