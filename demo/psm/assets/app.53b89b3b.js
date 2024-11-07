0ad6b25f21cfb8ea7f2ef82c0a98633037001564
J.sky <bosichong@qq.com>
2024-11-07 08:12:51 +0800

    更新博客

---
+++ demo/psm/assets/app.53b89b3b.js
@@ -0,0 +1,1 @@
+import{G as a}from"./index.00b9fb4b.js";const s=a("app",{state:()=>({printPreviewPapers:[]}),actions:{navigateToPrint(t,e,p){this.$patch(r=>{r.printPreviewPapers=p,t.push({path:"/print",query:{fileName:e}})})}}});export{s as u};
